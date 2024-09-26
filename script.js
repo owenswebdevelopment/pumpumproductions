3702f6b3668be6430d6a9ab9dbe135e5c70e0e92
Owen <139211753+owenswebdevelopment@users.noreply.github.com>
2024-09-26 16:50:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -41,23 +41,44 @@ const swiper = new Swiper('.slider-wrapper', {
   nav = document.querySelector("nav"),
   darkLightToggle = document.querySelector(".dark-light"),
   searchBoxToggle = document.querySelector(".searchBox"),
-  sidebarClose = document.querySelector(".sidebarOpen"),
-  sidebarOpen = document.querySelector(".sidebarClose");
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  sidebarClose = document.querySelector(".sidebarClose");
 
-  
-  darkLightToggle.addEventListener("click", ()=> {
+  let getMode = localStorage.getItem("mode");
+if(getMode && getMode === "dark-mode"){
+  body.classList.add("dark");
+} 
+//js code to toggle light mode to dark mode
+  darkLightToggle.addEventListener("click", () =>{
     darkLightToggle.classList.toggle("active");
     body.classList.toggle("dark");
 
+    if(body.classList.contains("dark")){
+      localStorage.setItem("mode", "dark-mode");
+    } else {
+      localStorage.setItem("mode", "light-mode");
+    }
+
   });
 
- searchBoxToggle.addEventListener("click", ()=> {
+ searchBoxToggle.addEventListener("click", () =>{
     searchBoxToggle.classList.toggle("active");
   
   });
   
 
   // js code to toggle sidebar
-  sidebarOpen.addEventListener("click", ()=> {
+  sidebarOpen.addEventListener("click", () =>{
     nav.classList.toggle("active");
-  })
+  });
+
+  sidebarClose.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+  });
+
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+   if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("sidebarClose")){
+    nav.classList.remove("active");
+   }
+})
